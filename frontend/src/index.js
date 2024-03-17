df3015ca4fadf3635fe4406e0ae53b15272889d7
Jared Braswell <jjb21l@fsu.edu>
2024-03-17 19:13:03 -0400

    Trying to create login page /login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import { 
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
+import store from './store';
+
+
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
         </Route>
     )
 )
@@ -21,7 +28,11 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+    <Provider store={store}>
         <RouterProvider router={router} />
+    </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
