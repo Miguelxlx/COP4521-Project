8ef170addb7fde811cd8719df22b74dae6554f10
Jared Braswell <jjb21l@fsu.edu>
2024-03-19 18:05:06 -0400

    Fixed errors

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 //import cartSliceReducer from './slices/betSlice';
-//import authSliceReducer from './slices/authSlice';
+import authSliceReducer from './slices/authSlice';
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         //bet: cartSliceReducer,
-        //auth: authSliceReducer,
+        auth: authSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
