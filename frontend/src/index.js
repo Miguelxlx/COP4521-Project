9d4e5a73e3362110af229daeee4596b7e5ec9375
Jared Braswell <jjb21l@fsu.edu>
2024-03-19 20:59:04 -0400

    Header and Footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
