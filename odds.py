2b04abdbc7789ddb4d86b9dc1526fe7b90dba0b7
osher-steel <oshersteel@gmail.com>
2024-04-02 13:23:47 -0400

    Added nba_api to get game results

--- odds.py
+++ odds.py
@@ -25,21 +25,29 @@ if odds_response.status_code != 200:
     print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
 else:
     odds_json = odds_response.json()
-    print(odds_json)
 
-    # # Parsing JSON data
-    # for game in odds_json:
-    #     time = game["commence_time"]
-    #     home_team = game["home_team"]
-    #     away_team = game["away_team"]
-    #     line = game["bookmakers"][0]["markets"][0]["outcomes"][0]["point"]
-    #     over_price = game["bookmakers"][0]["markets"][0]["outcomes"][0]["price"]
-    #     under_price = game["bookmakers"][0]["markets"][0]["outcomes"][1]["price"]
+    for game in odds_json:
+        time = game["commence_time"]
+        home_team = game["home_team"]
+        away_team = game["away_team"]
+        line = game["bookmakers"][0]["markets"][1]["outcomes"][0]["point"]
+        over_price = game["bookmakers"][0]["markets"][1]["outcomes"][0]["price"]
+        under_price = game["bookmakers"][0]["markets"][1]["outcomes"][1]["price"]
+
+        h2h1 = (game["bookmakers"][0]["markets"][0]["outcomes"][0]["name"],game["bookmakers"][0]["markets"][0]["outcomes"][0]["price"])
+        h2h2 = (game["bookmakers"][0]["markets"][0]["outcomes"][1]["name"],game["bookmakers"][0]["markets"][0]["outcomes"][1]["price"])
         
-    #     print("Time:", time)
-    #     print("Home Team:", home_team)
-    #     print("Away Team:", away_team)
-    #     print("Line:", line)
-    #     print("Over Price:", over_price)
-    #     print("Under Price:", under_price)
-    #     print("\n")
+        print("Time:", time)
+        print("Home Team:", home_team)
+        print("Away Team:", away_team)
+        print("Line:", line)
+        print("Over Price:", over_price)
+        print("Under Price:", under_price)
+
+        print(f"{h2h1[0]} to win: {h2h1[1]}")
+        print(f"{h2h2[0]} to win: {h2h2[1]}")
+        print("\n")
+
+    # Check the usage quota
+    print('Remaining requests', odds_response.headers['x-requests-remaining'])
+    print('Used requests', odds_response.headers['x-requests-used'])
