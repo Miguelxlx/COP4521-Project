a833ff4be574e2f357f0d1c8fd4fba0b7aa7092b
Jared Braswell <jjb21l@fsu.edu>
2024-04-18 15:30:49 -0400

    Odds added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -121,7 +121,7 @@ function App() {
         ) : (
           odds.map((odd, index) => (
             <div key={index} style={{ marginBottom: '20px' }}>
-              <h3>{odd.home_team} vs. {odd.visitor_team}</h3>
+              <h3>{odd.home_team} ({odd.h2h_home_price}) vs. {odd.visitor_team} ({odd.h2h_visitor_price}) </h3>
               <h4>Over: {odd.over_price} Under: {odd.under_price}</h4>
               <h5>Line: {odd.line}</h5>
               <select
