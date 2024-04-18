7d7bf9b7c430f8a6b838aff52e7572cd451c4470
Jared Braswell <jjb21l@fsu.edu>
2024-04-18 15:23:21 -0400

    UserListScreen

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 const AdminRoute = () => {
     const {userInfo} = useSelector(state => state.auth) 
   return (
-    userInfo && userInfo.isAdmin ? <Outlet /> : <Navigate to="/login" replace />
+    userInfo && userInfo.role ? <Outlet /> : <Navigate to="/login" replace />
   )
 };
 
